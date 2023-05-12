2b5d338b160c4b1bec9f3dac26c583d89ef492a4
Antoooony <81672358+Antoooony@users.noreply.github.com>
2023-05-12 15:09:49 +0200

    Route /factures
    génération facture commande par id_user

--- apiprojet/src/main/java/com/example/apiprojet/api/model/Facture.java
+++ apiprojet/src/main/java/com/example/apiprojet/api/model/Facture.java
@@ -5,31 +5,108 @@ public class Facture {
     private String Num_commande;
     private String Datefacture;
     private String Datecommande;
+
+    private String Qte;
     private String Nom;
     private String Prenom;
-
     private String Telephone;
-    private String Adresse;
+    private String CP;
     private String Ville;
-    private String Code_postal;
+    private String adresse;
     private String Libelle;
     private String Prix;
-    private String Qte;
 
-    public Facture(String Num_commande, String Datefacture,String Datecommande, String Nom, String Prenom, String Telephone, String Adresse, String Ville, String Code_postal, String Libelle, String Prix, String Qte) {
 
+    public Facture(String Num_commande, String Datefacture, String Datecommande, String Qte, String Nom, String Prenom, String Telephone, String CP, String Ville, String adresse, String Libelle, String Prix) {
         this.Num_commande = Num_commande;
         this.Datefacture = Datefacture;
         this.Datecommande = Datecommande;
+        this.Qte = Qte;
         this.Nom = Nom;
         this.Prenom = Prenom;
         this.Telephone = Telephone;
-        this.Adresse = Adresse;
+        this.CP = CP;
         this.Ville = Ville;
-        this.Code_postal = Code_postal;
+        this.adresse = adresse;
         this.Libelle = Libelle;
         this.Prix = Prix;
+    }
+
+
+    public String getNum_commande() {
+        return Num_commande;
+    }
+    public void setNum_commande(String Num_commande) {
+        this.Num_commande = Num_commande;
+    }
+    public String getDatefacture() {
+        return Datefacture;
+    }
+    public void setDatefacture(String Datefacture) {
+        this.Datefacture = Datefacture;
+    }
+    public String getDatecommande() {
+        return Datecommande;
+    }
+    public void setDatecommande(String Datecommande) {
+        this.Datecommande = Datecommande;
+    }
+    public String getQte() {
+        return Qte;
+    }
+    public void setQte(String Qte) {
         this.Qte = Qte;
     }
+    public String getNom() {
+        return Nom;
+    }
+    public void setNom(String Nom) {
+        this.Nom = Nom;
+    }
+    public String getPrenom() {
+        return Prenom;
+    }
+    public void setPrenom(String Prenom) {
+        this.Prenom = Prenom;
+    }
+    public String getTelephone(String Telephone) {
+        return Telephone;
+    }
+    public void setTelephone(String Telephone) {
+        this.Telephone = Telephone;
+    }
+    public String getCP() {
+        return CP;
+    }
+    public void setCP(String CP) {
+        this.CP = CP;
+    }
+    public String getVille() {
+        return Ville;
+    }
+    public void setVille(String Ville) {
+        this.Ville = Ville;
+    }
+    public String getAdresse() {
+        return adresse;
+    }
+    public void setAdresse(String adresse) {
+        this.adresse = adresse;
+    }
+
+    public String getLibelle() {
+        return Libelle;
+    }
+    public void setLibelle(String Libelle) {
+        this.Libelle = Libelle;
+    }
+    public String getPrix() {
+        return Prix;
+    }
+    public void setPrix(String Prix) {
+        this.Prix = Prix;
+    }
+    
+
 
 }
--- apiprojet/src/main/java/com/example/apiprojet/service/FactureService.java
+++ apiprojet/src/main/java/com/example/apiprojet/service/FactureService.java
@@ -18,7 +18,7 @@ public class FactureService {
 
             ResultSet resultSet = dbconnect.getStatement().executeQuery("SELECT f.Num_commande,f.date, c.date, q.Qte, u.nom, u.prenom, u.telephone, r.CP, r.Ville, r.adresse, a.libelle, a.prix FROM facture f, commandes c, qtearticle q, utilisateur u, article a, adresse r WHERE f.Num_commande = c.Num_commande AND c.Num_commande = q.Num_commande AND q.Article_ID= a.Article_ID AND u.id_user= r.id_user AND u.id_user = c.id_user");
             while (resultSet.next()) {
-factures.add(new Facture(resultSet.getString("Num_commande"), resultSet.getString("date"), resultSet.getString("date"), resultSet.getString("Qte"), resultSet.getString("nom"), resultSet.getString("prenom"), resultSet.getString("telephone"), resultSet.getString("CP"), resultSet.getString("Ville"), resultSet.getString("adresse"), resultSet.getString("libelle"), resultSet.getString("prix")));
+                factures.add(new Facture(resultSet.getString("Num_commande"), resultSet.getString("date"), resultSet.getString("date"), resultSet.getString("Qte"), resultSet.getString("nom"), resultSet.getString("prenom"), resultSet.getString("telephone"), resultSet.getString("CP"), resultSet.getString("Ville"), resultSet.getString("adresse"), resultSet.getString("libelle"), resultSet.getString("prix")));
             }
 
         }
